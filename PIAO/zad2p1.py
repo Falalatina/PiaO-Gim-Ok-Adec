d86a0105922aefe4092bd313ab6e59337ce20df1
Falalatina <karolina40.gorska17@gmail.com>
2024-03-13 19:46:32 +0100

    add

--- PIAO/zad2p1.py
+++ PIAO/zad2p1.py
@@ -2,34 +2,34 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 # Wczytanie obrazu
-image_path = "rozmyte.jpg"
+image_path = "rozmkr.jpg"
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 # Wyświetlenie obrazu
-plt.imshow(image, cmap='gray')
-plt.title('Original Image')
-plt.axis('off')
-plt.show()
+# plt.imshow(image, cmap='gray')
+# plt.title('Original Image')
+# plt.axis('off')
+# plt.show()
 
 # Dyskretna transformaty Fouriera (DFT)
 dft = np.fft.fft2(image)
 dft_shift = np.fft.fftshift(dft)
 # Wyświetlenie widma częstotliwościowego
-magnitude_spectrum = 20 * np.log(np.abs(dft_shift))
-plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Magnitude Spectrum')
-plt.axis('off')
-plt.show()
+magnitude_spectrum = 10 * np.log(np.abs(dft_shift))
+# plt.imshow(magnitude_spectrum, cmap='gray')
+# plt.title('Magnitude Spectrum')
+# plt.axis('off')
+# plt.show()
 
 
 # Tworzenie maski dolnoprzepustowej
 rows, cols = image.shape
-crow, ccol = rows // 4, cols // 4
+crow, ccol = rows // 2 , cols //  2
 mask = np.zeros((rows, cols), np.uint8)
-r = 800
+r = 10000
 center = [crow, ccol]
 x, y = np.ogrid[:rows, :cols]
 mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 <= r*r
-mask[mask_area] = 10
+mask[mask_area] = 2
 # Filtracja w dziedzinie częstotliwości
 fshift = dft_shift * mask
 f_ishift = np.fft.ifftshift(fshift)
